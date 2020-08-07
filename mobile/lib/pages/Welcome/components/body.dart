fe583a00d53c503a5441ba08ccc4af4c0da8f671
Henrique Martins <hmartins224@gmail.com>
2020-08-07 17:46:23 -0300

    feat(mobile): creating a widget for the background

--- mobile/lib/pages/Welcome/components/body.dart
+++ mobile/lib/pages/Welcome/components/body.dart
@@ -1,31 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/pages/Welcome/components/background.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/img/main_top.png",
-              width: size.width * 0.38,
-            )
+          Text(
+            "Bem vindo",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/img/main_bottom.png",
-              width: size.width * 0.23,
-            ),
-          )
         ],
       ),
     );
