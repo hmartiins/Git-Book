c8038f1725ab9b3fce0531b55f4faab6e6e84c7d
Henrique Martins <hmartins224@gmail.com>
2020-08-09 11:09:29 -0300

    feat(mobile): abstracting the body and the background in widgets

---
+++ mobile/lib/pages/Login/components/body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/pages/Login/components/background.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Background(
+      child: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
--- mobile/lib/pages/Login/login_screen.dart
+++ mobile/lib/pages/Login/login_screen.dart
@@ -1,35 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/main.dart';
+import 'package:mobile/pages/Login/components/body.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: size.height,
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Positioned(
-                top: 0,
-                left: 0,
-                child: Image.asset(
-                  "assets/img/main_top.png",
-                  width: size.width * 0.36,
-                )),
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: Image.asset(
-                "assets/img/login_bottom.png",
-                width: size.width * 0.6,
-              ),
-            )
-          ],
-        ),
-      ),
+      body: Body(),
     );
   }
 }
