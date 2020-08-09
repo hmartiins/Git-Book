fe2c5b6fba4ded03ffc79bca2d8ff10b60c24430
Henrique Martins <hmartins224@gmail.com>
2020-08-09 11:19:08 -0300

    style(mobile): adding assets on the login screen

--- mobile/lib/pages/Login/components/body.dart
+++ mobile/lib/pages/Login/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mobile/pages/Login/components/background.dart';
 
 class Body extends StatelessWidget {
@@ -8,9 +9,24 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Background(
       child: Column(
-        children: <Widget>[],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "LOGIN",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 26,
+            ),
+          ),
+          SvgPicture.asset(
+            "assets/img/loginAsset.svg",
+            height: size.height * 0.38,
+          )
+        ],
       ),
     );
   }
