15e0413caf35115c71c885ce4029d325347f188f
Henrique Martins <hmartins224@gmail.com>
2020-08-09 10:59:20 -0300

    style(mobile): creating background for login screen

--- mobile/lib/pages/Login/login_screen.dart
+++ mobile/lib/pages/Login/login_screen.dart
@@ -4,6 +4,32 @@ import 'package:mobile/main.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+                top: 0,
+                left: 0,
+                child: Image.asset(
+                  "assets/img/main_top.png",
+                  width: size.width * 0.36,
+                )),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Image.asset(
+                "assets/img/login_bottom.png",
+                width: size.width * 0.6,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
