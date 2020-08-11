9ca5212528a174f7b775f52739bdad1324e74a1b
João Victor <jv782063@gmail.com>
2020-08-10 21:31:12 -0300

    feat(server): creates user migration
    creates user migration with knex

---
+++ server/src/database/migrations/20200810211738_createUser.js
@@ -0,0 +1,15 @@
+exports.up = (knex) =>
+  knex.schema.createTable('tb_user', (table) => {
+    table.increments('cd_user');
+
+    table.string('name', 45).notNullable();
+    table.string('email', 100).notNullable().unique();
+    table.string('password').notNullable();
+
+    table.date('birth');
+    table.string('telephone', 25).notNullable();
+    table.string('cpf', 45).notNullable().unique();
+    table.string('rg', 45).notNullable().unique();
+  });
+
+exports.down = (knex) => knex.schema.dropTable('tb_user');
