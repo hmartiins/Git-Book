edf0a481143733c0829d08a1127abd4b31076a27
Henrique Martins <hmartins224@gmail.com>
2020-08-11 20:23:38 -0300

    feat(server): creating a development server
    Creating a development server running on port 3333

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,12 @@
-console.log('start');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(3333);
