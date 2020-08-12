10cb6f9d14c215518bf770accbf1806e414a2904
Henrique Martins <hmartins224@gmail.com>
2020-08-12 20:18:06 -0300

    feat(server): creating update route the user

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -28,7 +28,7 @@ class UserController {
       await trx('tb_user').insert(user);
       await trx.commit();
 
-      return response.status(200).json({
+      return response.status(201).json({
         succes: 'Registered successfully',
         data: user,
       })
@@ -72,7 +72,40 @@ class UserController {
         success: `successfully deleting the user with id ${cd_user}`
       });
     } catch (err) {
+      console.log(err)
 
+      return response.status(400).send({ error: 'error when deleting the record' })
+    }
+  }
+  async update(request, response) {
+    const { cd_user } = request.params
+
+    const {
+      name,
+      email,
+      password
+    } = request.body
+
+    try {
+      const userVerification = await knex('tb_user')
+        .where('cd_user', cd_user)
+        .first()
+        .select('cd_user')
+
+      if (!userVerification) {
+        return response.status(400).send({ success: 'User not found' })
+      }
+
+      await knex('tb_user').update({
+        name,
+        email,
+        password
+      }).where({ cd_user });
+
+      return response.status(200).send({ ok: 'Updated successfully' })
+    } catch (err) {
+      console.log(err)
+      response.status(400).send({ error: 'Failed to update' })
     }
   }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -7,7 +7,8 @@ const UserController = require('./controllers/UserController');
 const userController = new UserController();
 
 routes.post('/user', userController.create);
-routes.get('/user/:cd_user', userController.show)
-routes.delete('/user/:cd_user', userController.delete)
+routes.get('/user/:cd_user', userController.show);
+routes.delete('/user/:cd_user', userController.delete);
+routes.put('/user/:cd_user', userController.update);
 
 module.exports = routes;
