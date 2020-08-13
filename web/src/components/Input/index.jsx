4345cbd74ad098a997e6722f1f89e5afaee6d3ac
João Victor <jv782063@gmail.com>
2020-08-13 20:18:24 -0300

    feat(web): creates the input component along with your css style

--- web/src/components/Input/index.jsx
+++ web/src/components/Input/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from './styles.module.css';
 
-
-export default props =>
-    <>
-        <input type={props.type} name={props.name} placeholder={props.placeholder} />
-
-    </>
+export default function Footer(props) {
+    return (
+      <div className={styles.inputGroup}>
+        <label for={props.name}>
+          {props.label}
+          {props.mandatory ? <span>*</span> : null}
+        </label>
+        <input
+          type={props.type}
+          name={props.name}
+          placeholder={props.placeholder}
+        />
+      </div>
+    );
+}
