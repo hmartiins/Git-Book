6bad107bbddd671fc30b4f79141b02e376786ae8
hmartiins <hmartins224@gmail.com>
2020-08-23 12:50:45 -0300

    refactor(server): changing the address field of tb_address, removing its uniqueness

--- server/src/models/migrations/20200812164335_createAdress.js
+++ server/src/models/migrations/20200812164335_createAdress.js
@@ -4,7 +4,7 @@ exports.up = (knex) =>
 		table.increments('cd_adress');
 
 		table.string('cep', 20).notNullable();
-		table.string('adress', 100).notNullable().unique();
+		table.string('adress', 100).notNullable();
 		table.string('comp_adress', 10).notNullable();
 		table.integer('id_user').unsigned();
 		table.foreign('id_user').references('cd_user').inTable('tb_user');
