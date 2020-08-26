7d5117bcff202e2b3a2b991ea5845093771a7995
hmartiins <hmartins224@gmail.com>
2020-08-25 21:37:27 -0300

    feat(server): adding the azure storage account connectionstring key

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,5 +2,6 @@
 const md5HashKey = '508a564003c578d429792581f1759074';
 module.exports = {
   md5HashKey,
-  sendGridKey: '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'
+  sendGridKey: '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',
+  containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=gitbook;AccountKey=Lp9JRUzfRQpuVxaD8UBTJjn0QYp1KzvOxhnsnc3irUSnJZCMA0Fj7Hmg0KjBxX/UUMDzAvWrVhVEMJgs2a5ufA==;EndpointSuffix=core.windows.net',
 }
