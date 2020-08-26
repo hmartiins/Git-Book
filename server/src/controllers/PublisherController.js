27d73c5f28ab9976ff1ee3806cf9c6c98bd6dd33
hmartiins <hmartins224@gmail.com>
2020-08-26 09:50:17 -0300

    feat(server): starter PublisherController development

---
+++ server/src/controllers/PublisherController.js
@@ -0,0 +1,48 @@
+const knex = require('../models/connection');
+const config = require('../config/config');
+const azure = require('azure-storage');
+const crypto = require('crypto');
+
+class PublisherController {
+  async create(request, response) {
+    const {
+      publisher
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    try {
+      const blobService = azure.createBlobService(config.containerConnectionString);
+
+      let fileName = crypto.randomBytes(20).toString('hex') + '.png';
+      const rawDataImage = request.body.image;
+      const matches = rawDataImage.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+      const type = matches[1];
+      const buffer = new Buffer.alloc(matches[2], 'base64');
+
+      await blobService.createBlockBlobFromText('publisher-logo', fileName, buffer, {
+        contentType: type
+      }, (err, result, response) => {
+        if (err) {
+          fileName = 'default-publisher.png'
+        }
+      });
+
+      console.log(publisher, fileName);
+
+      await trx('tb_publisher').insert(publisher, fileName);
+      await trx.commit();
+
+      return response.status(201).json({
+        succes: 'Registered successfully',
+        data: publisher, publisher_logo,
+      });
+    } catch (err) {
+      console.log(err);
+      response.status(400).send({ error: 'Failed to register' });
+    }
+
+  }
+}
+
+module.exports = PublisherController;
--- server/src/routes.js
+++ server/src/routes.js
@@ -4,9 +4,11 @@ const routes = Router();
 
 const UserController = require('./controllers/UserController');
 const AddressController = require('./controllers/AddressController');
+const PublisherController = require('./controllers/PublisherController');
 
 const userController = new UserController();
 const addressController = new AddressController();
+const publisherController = new PublisherController();
 
 routes.post('/user', userController.create);
 routes.get('/user/:cd_user', userController.show);
@@ -14,5 +16,7 @@ routes.delete('/user/:cd_user', userController.delete);
 routes.put('/user/:cd_user', userController.update);
 
 routes.post('/address', addressController.create);
+
+routes.post('/publisher', publisherController.create);
 
 module.exports = routes;
