6f6c7b598abbcbc387fc81a0b0876fad29ad2641
Henrique Martins <hmartins224@gmail.com>
2020-08-31 16:34:49 -0300

    feat(server): adding assets, entries and the button to continue, on the registration screen

--- mobile/lib/pages/Register/components/body.dart
+++ mobile/lib/pages/Register/components/body.dart
@@ -4,7 +4,6 @@ import 'package:mobile/components/button.dart';
 import 'package:mobile/components/input_field.dart';
 import 'package:mobile/components/password_field.dart';
 import 'package:mobile/components/text_account_check.dart';
-import 'package:mobile/constants.dart';
 import 'package:mobile/pages/Login/components/background.dart';
 
 class Body extends StatelessWidget {
@@ -23,14 +22,40 @@ class Body extends StatelessWidget {
             height: size.height * 0.02,
           ),
           Text(
-            "LOGIN",
+            "CADASTRO",
             style: TextStyle(
               fontWeight: FontWeight.bold,
               color: Colors.white,
               fontSize: 26,
             ),
           ),
-          TextAccountCheck()
+          SizedBox(
+            height: size.height * 0.06,
+          ),
+          SvgPicture.asset(
+            "assets/img/bookLover.svg",
+            height: size.height * 0.3,
+          ),
+          InputField(
+            hintText: "Seu nome",
+            onChanged: (value) {},
+            icon: Icons.face,
+          ),
+          InputField(
+            hintText: "Seu email",
+            onChanged: (value) {},
+            icon: Icons.email,
+          ),
+          PasswordField(
+            onChanged: (vl) {},
+          ),
+          SizedBox(
+            height: size.height * 0.02,
+          ),
+          Button(
+            textButton: "CONTINUAR",
+            handlePressButton: () {},
+          ),
         ],
       ),
     );
