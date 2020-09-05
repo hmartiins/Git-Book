c31ab8568e5a72c106ffdab5feb5cdd21f705450
Henrique Martins <hmartins224@gmail.com>
2020-09-05 16:38:38 -0300

    feat(mobile): starting development of the finishRegister page

--- mobile/lib/pages/FinishRegister/components/body.dart
+++ mobile/lib/pages/FinishRegister/components/body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mobile/components/button.dart';
 import 'package:mobile/components/input_field.dart';
-import 'package:mobile/components/password_field.dart';
 import 'package:mobile/components/text_account_check.dart';
 import 'package:mobile/pages/FinishRegister/finish_register_screen.dart';
 import 'package:mobile/pages/Login/components/background.dart';
@@ -35,27 +34,19 @@ class Body extends StatelessWidget {
             height: size.height * 0.04,
           ),
           SvgPicture.asset(
-            "assets/img/bookLover.svg",
+            "assets/img/finishChek.svg",
             height: size.height * 0.3,
           ),
           InputField(
-            hintText: "Seu nome",
+            hintText: "Lorem",
             onChanged: (value) {},
             icon: Icons.face,
-          ),
-          InputField(
-            hintText: "Seu email",
-            onChanged: (value) {},
-            icon: Icons.email,
-          ),
-          PasswordField(
-            onChanged: (vl) {},
           ),
           SizedBox(
             height: size.height * 0.017,
           ),
           Button(
-            textButton: "CONTINUAR",
+            textButton: "FINALIZAR",
             handlePressButton: () {
               Navigator.push(
                 context,
