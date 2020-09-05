bdf5c9865e3118ebcbc65769f43dc7d897acdbe9
João Victor <jv782063@gmail.com>
2020-09-05 18:20:57 -0300

    refactor(server): changes a category migration property to a single one

--- server/src/models/migrations/20200814151140_createCategory.js
+++ server/src/models/migrations/20200814151140_createCategory.js
@@ -1,10 +1,9 @@
 exports.up = (knex) =>
-	knex.schema.createTable('tb_category', (table) => {
+  knex.schema.createTable('tb_category', (table) => {
+    table.increments('cd_category');
 
-		table.increments('cd_category');
-
-		table.string('category', 30).notNullable();
-		table.text('category_description').notNullable();
-	});
+    table.string('category', 30).unique().notNullable();
+    table.text('category_description').notNullable();
+  });
 
 exports.down = (knex) => knex.schema.dropTable('cd_category');
