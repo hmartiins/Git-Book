ef2c8df08ead859f3369663684bb7afeded68e23
João Victor <jv782063@gmail.com>
2020-09-14 20:53:03 -0300

    refactor(web): adjusts the problems that the inputs were having

--- web/src/components/Input/index.jsx
+++ web/src/components/Input/index.jsx
@@ -1,33 +1,20 @@
-import React from 'react'
+import React from 'react';
 import styles from './styles.module.css';
-
-import * as FiIcons from 'react-icons/fi';
 
 function Input(props) {
   const style = {
-    width: props.width
+    width: props.width,
   };
 
-  const icon = props.iconPrefix;
-
-  console.log(icon);
-
-  const FiIcon = FiIcons[icon];
   return (
     <div className={styles.inputGroup}>
-      <FiIcon />
       <label htmlFor={props.name}>
         {!props.label ? null : props.label}
         {props.mandatory ? <span>*</span> : null}
       </label>
-      <input
-        type={props.type}
-        name={props.name}
-        placeholder={props.placeholder}
-        style={style}
-      />
+      <input type={props.type} name={props.name} placeholder={props.placeholder} style={style} />
     </div>
   );
 }
 
-export { Input }
+export { Input };
--- web/src/pages/Login/index.jsx
+++ web/src/pages/Login/index.jsx
@@ -1,41 +1,49 @@
 import React from 'react';
 import { Input } from '../../components/Input/index';
 
+import styles from './styles.module.css';
+
 export default function Login() {
   return (
     <>
-      <Input
-        name="name"
-        iconPrefix="FiAirplay"
-        label="Seu Nome"
-        mandatory={true}
-        type="text"
-        placeholder="Nome Completo"
-        width="470px"
-      />
-      {/* <Input
-        name="email"
-        label="E-mail"
-        mandatory={true}
-        type="email"
-        placeholder="seu@email.com"
-        width="470px"
-      />
-      <Input
-        name="senha"
-        label="Senha"
-        mandatory={true}
-        type="password"
-        placeholder="pelo menos 6 caracteres"
-      />
-      <Input
-        name="confirmsenha"
-        label="Insira a senha novamente"
-        mandatory={true}
-        type="password"
-        placeholder="confirme sua senha"
-      /> */}
-
+      <div className={styles.inputGroup}>
+        <Input
+          name="name"
+          label="Seu Nome"
+          mandatory={true}
+          type="text"
+          placeholder="Nome Completo"
+          width="470px"
+        />
+        <Input
+          name="email"
+          label="E-mail"
+          mandatory={true}
+          type="email"
+          placeholder="seu@email.com"
+          width="470px"
+        />
+      </div>
+      <div className={styles.inputGroup}>
+        <Input
+          name="senha"
+          label="Senha"
+          mandatory={true}
+          type="password"
+          placeholder="pelo menos 6 caracteres"
+          width="470px"
+        />
+      </div>
+      <div className={styles.inputGroup}>
+        <Input
+          name="confirmsenha"
+          label="Insira a senha novamente"
+          mandatory={true}
+          type="password"
+          placeholder="confirme sua senha"
+          width="470px"
+        />
+      </div>
     </>
   );
 }
